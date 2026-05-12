547849cee9c5ad25304d33d52139faaae64d7dc5
אלי שיינפלד <schonfeld@MacBook-Air-sl-ly.local>
2026-05-12 13:13:47 +0300

    Add GitHub updater metadata

--- colt-experience.php
+++ colt-experience.php
@@ -1,17 +1,20 @@
 <?php
 /**
  * Plugin Name: COLT Experience
+ * Plugin URI: https://github.com/Astratego-dev/colt
  * Description: Premium shortcode-driven experience pages for the COLT collectibles site.
- * Version: 0.1.0
+ * Version: 0.1.1
  * Author: COLT
  * Text Domain: colt-experience
+ * GitHub Plugin URI: https://github.com/Astratego-dev/colt
+ * Primary Branch: main
  */
 
 if (!defined('ABSPATH')) {
     exit;
 }
 
-define('COLT_EXPERIENCE_VERSION', '0.1.0');
+define('COLT_EXPERIENCE_VERSION', '0.1.1');
 define('COLT_EXPERIENCE_FILE', __FILE__);
 define('COLT_EXPERIENCE_DIR', plugin_dir_path(__FILE__));
 define('COLT_EXPERIENCE_URL', plugin_dir_url(__FILE__));
